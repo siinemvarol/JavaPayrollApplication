fade4861a6326303795ed7e0ede549b846eb6f57
siinemvarol <siinemvarol@gmail.com>
2024-03-24 20:34:12 +0000

    Entities created for payroll application

---
+++ src/main/java/com/sinem/repository/entity/Employee.java
@@ -0,0 +1,11 @@
+package com.sinem.repository.entity;
+
+abstract class Employee {
+    private String name;
+    private String surname;
+
+    public void calculateWage() {
+
+    }
+
+}
---
+++ src/main/java/com/sinem/util/ReadFile.java
@@ -0,0 +1,4 @@
+package com.sinem.util;
+
+public class ReadFile {
+}
