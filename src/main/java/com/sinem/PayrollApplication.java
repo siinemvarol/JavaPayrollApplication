e47135794905aa98a016b6b7d876e97905ffef07
siinemvarol <siinemvarol@gmail.com>
2024-03-27 23:22:56 +0000

    Printing payrolls feature is added

--- src/main/java/com/sinem/PayrollApplication.java
+++ src/main/java/com/sinem/PayrollApplication.java
@@ -7,6 +7,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -17,19 +18,27 @@ public class PayrollApplication {
     public static void main(String[] args) {
         ReadFile.readJsonFile("employee.json");
 
-        System.out.println("Employees who worked less than 150 hours: ");
+        System.out.println("\nEmployees who worked less than 150 hours: ");
+        System.out.println("- - - - - - - - - - - - - - - - - - - - -");
         if(ReadFile.lessThan150WorkingHourList.size() > 0) {
             System.out.println(ReadFile.lessThan150WorkingHourList.toString());
         } else {
             System.out.println("All employees have worked more than 150 hours this month!");
         }
 
-        try (FileWriter file = new FileWriter("all_payrolls.json")) {
+   /*     try (FileWriter file = new FileWriter("all_payrolls.json")) {
             file.write(Payroll.payrollList.toString());
             file.flush();
 
         } catch (IOException e) {
          e.printStackTrace();
+        }
+    */
+
+        System.out.println("\nMARCH 2024 PAYROLLS");
+        System.out.println("- - - - - - - - - -");
+        for (Object eachPayroll : Payroll.payrollList) {
+            System.out.println(eachPayroll.toString());
         }
 
     }
